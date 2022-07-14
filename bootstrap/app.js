70dca1511f6ac862880cb26184efb3fdc77da4e7
Arnau-Mas-ASUS <arnau.mava@gmail.com>
2022-07-14 13:49:39 +0200

    first update exercici web

---
+++ bootstrap/app.js
@@ -0,0 +1,8 @@
+let videojuegos = ["Final fantasy", "Fifa", "fall guys"];
+
+let parrafo = document.querySelector("#videojuegos");
+parrafo.textContent = videojuegos;
+
+function sumar(){
+    document.querySelector("#numero").textContent = ++;
+}
